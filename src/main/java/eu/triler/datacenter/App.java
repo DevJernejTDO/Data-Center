4b1b25cb9f92c40234051720110daa0bede9579c
JernejTDO <jernej.mip@gmail.com>
2023-09-01 16:45:50 +0200

    Added CreateLocalFiles

--- src/main/java/eu/triler/datacenter/App.java
+++ src/main/java/eu/triler/datacenter/App.java
@@ -1,30 +1,52 @@
 package eu.triler.datacenter;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import eu.triler.datacenter.Data.DataManager;
 import eu.triler.datacenter.Data.ServerData;
 import myTools.FileReader;
+import myTools.JsonManager;
 
 public class App {
 
     public static Map<String, ServerData> ServersMap = new HashMap<>();
 
     private static App thisApp;
+    private static DataManager dataManager = null;
     private static FileReader reader = null;
+    private static JsonManager jsonManager = null;
+    private static String mainPath = null;
 
     public static void main(String[] args) {
         thisApp = new App();
+        dataManager = new DataManager();
         reader = new FileReader();
+        jsonManager = new JsonManager();
+        mainPath = (new File("").getAbsolutePath());
 
-        thisApp.loadData();
+        getApp().getDataManager().CreateLocalFiles();
+        ;
     }
 
-    public void loadData() {
-        String DefaultConfig = getReader().getTextFromFile("config.yml");
+    public static App getApp() {
+        return thisApp;
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
     }
 
     public FileReader getReader() {
         return reader;
+    }
+
+    public JsonManager getJsonManager() {
+        return jsonManager;
+    }
+
+    public String getPath() {
+        return mainPath;
     }
 }
---
+++ src/main/java/eu/triler/datacenter/Data/DataManager.java
@@ -0,0 +1,27 @@
+package eu.triler.datacenter.Data;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import eu.triler.datacenter.App;
+import myTools.FileReader;
+import myTools.JsonManager;
+
+public class DataManager {
+
+    public void CreateLocalFiles() {
+        App myApp = App.getApp();
+        FileReader reader = myApp.getReader();
+        JsonManager json = myApp.getJsonManager();
+
+        // Gett all defauld files
+        String defauldConfig = reader.getTextFromFile("config.yml");
+
+        // Set string to JSON
+        JsonObject defaultConfigObject = (JsonObject) JsonParser.parseString(defauldConfig);
+
+        // Create Files
+        json.createJsonFile("config", defaultConfigObject, myApp.getPath());
+    }
+
+}
